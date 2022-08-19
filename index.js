57c8617605229fd586797bf84931eec2705cc71d
bsprague6 <bsprague6@gmail.com>
2022-08-19 14:27:02 -0400

    Testing

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ passport.deserializeUser(function(obj, cb) {
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+console.log("homeUrl is: " + homeUrl);
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
